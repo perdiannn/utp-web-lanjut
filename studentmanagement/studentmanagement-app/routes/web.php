f99e0b2d6b598cfbcef90b75e31485b9c9b3d3d2
Ferdian <ferdi4nn@gmail.com>
2025-05-06 14:50:04 +0700

    Part 6

--- studentmanagement/studentmanagement-app/routes/web.php
+++ studentmanagement/studentmanagement-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
@@ -22,3 +23,5 @@ Route::get('/', function () {
 Route::resource('/students', StudentController::class);
 
 Route::resource('/teachers', TeacherController::class);
+
+Route::resource('/courses', CourseController::class);
