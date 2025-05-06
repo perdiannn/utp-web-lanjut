82f50254193f221b530b9ef46fd000fa3cfbdfb0
Ferdian <ferdi4nn@gmail.com>
2025-05-06 15:57:56 +0700

    Part 7

--- studentmanagement/studentmanagement-app/routes/web.php
+++ studentmanagement/studentmanagement-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BatchController;
 use App\Http\Controllers\CourseController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
@@ -25,3 +26,5 @@ Route::resource('/students', StudentController::class);
 Route::resource('/teachers', TeacherController::class);
 
 Route::resource('/courses', CourseController::class);
+
+Route::resource('/batches', BatchController::class);
